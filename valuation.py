0019514beaea571cb0185a6dd754e394aeae78b5
Mudit Tyagi <mudittyagi@Mudits-MacBook-Air.local>
2023-10-08 15:50:08 +0530

    add turn(white|black)

--- valuation.py
+++ valuation.py
@@ -4,10 +4,13 @@ import chess.engine
 def evaluate_position(board):
     if board is not None:
         with chess.engine.SimpleEngine.popen_uci("/opt/homebrew/bin/stockfish") as engine:
-            info = engine.analyse(board, chess.engine.Limit(depth=3))
+            info = engine.analyse(board, chess.engine.Limit(depth=10))
             score = info["score"].relative.score()
+            # 1 for white's turn
+            turn = 0 if (board.turn == chess.BLACK) else 1
+            
             # avoiding relative scores by stockfish
-            if board.turn == chess.BLACK:
+            if turn == 0:
                 score = -score
-            score = "{:.2f}".format(score/100)
-            return score
+            score = "{:.3f}".format(score/100)
+            return turn, score
